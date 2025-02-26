b34a056a28ee3769e42e79c19a710883e488c67d
Its-SuperNova <its.ashwin.23@gmail.com>
2025-02-26 09:18:07 +0530

    build erroe fixed

--- src/components/CookingCourse/index.tsx
+++ src/components/CookingCourse/index.tsx
@@ -1,30 +1,32 @@
-"use client"
-import React from "react";
-import Player from "react-lottie-player";
-import animationData from "../json/Fried Egg.json"; 
-import ShinyText from "../ui/shinyText";
+"use client";
+import React, { useEffect, useState } from "react";
+import animationData from "../json/Fried Egg.json";
+import dynamic from "next/dynamic";
+
+const Player = dynamic(() => import("react-lottie-player"), { ssr: false });
 
 const CourseNotFound = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-start w-full mt-[-30px]">
       <div className="w-full flex justify-center mb-5">
-        <Player
-          play
-          loop
-          animationData={animationData}
-          className="w-full max-w-[500px] h-auto  sm:mt-24"
-        />
+        {isClient && (
+          <Player
+            play
+            loop
+            animationData={animationData}
+            className="w-full max-w-[500px] h-auto md:max-w-[350px] md:max-h-[350px] sm:max-w-[250px] sm:max-h-[250px] sm:mt-24"
+          />
+        )}
       </div>
-      <h2 className="text-[26px] text-center px-5 mt-[-50px] w-[800px]">
-        <ShinyText
-          colors={["black", "#616161", "#B6B6B6", "#616161", "black"]}
-          animationSpeed={3}
-          showBorder={false}
-          className="custom-class"
-        >
-          I&apos;m cooking up something awesome—stay tuned while these pages get
-          prepped, seasoned, and served fresh!
-        </ShinyText>
+      <h2 className="text-[26px] text-gray-400 text-center px-5 mt-[-50px] w-[800px]">
+        I&apos;m cooking up something awesome—stay tuned while these pages get
+        prepped, seasoned, and served fresh!
       </h2>
     </div>
   );
